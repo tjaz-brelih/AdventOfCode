d205b3ec9263113bdf562899a0c8c4503f7b1519
Tjaž Brelih <tjaz.brelih@kolektor.com>
2019-12-13 14:22:10 +0100

    Day12

---
+++ AdventOfCode2019/Day12/Day12.fs
@@ -0,0 +1,19 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let rec readlines (filepath: string) = seq {
+    use sr = new StreamReader (filepath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+
+let parseLine line =
+    let matches = Regex.Matches(line, "[\d-]+")
+    (matches.[0], matches.[1], matches.[2])
+
+[<EntryPoint>]
+let main _ =
+    let niki = "input.txt" |> readlines |> parseLine
+
+    0
