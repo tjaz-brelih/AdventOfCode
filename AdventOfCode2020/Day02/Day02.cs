c75732c944b49355f07816a13af4b1746970fa17
Tjaž Brelih <tjaz.brelih@gmail.com>
2020-12-02 10:16:31 +0100

    Day 2 solutions.

---
+++ AdventOfCode2020/Day02/Day02.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Day02
+{
+    public class Day02
+    {
+        public static void Main()
+        {
+            var file = new StreamReader("input.txt");
+            string line;
+
+            List<Policy> policies = new();
+
+            while ((line = file.ReadLine()) is not null)
+            {
+                var span = line.AsSpan();
+
+                var separatorIndex = span.IndexOf('-');
+                var spaceIndex = span.IndexOf(' ');
+
+                var min = span[0..separatorIndex].ToString();
+                var max = span[(separatorIndex + 1)..spaceIndex].ToString();
+
+                var letter = span[spaceIndex + 1];
+
+                var password = span[(spaceIndex + 4)..];
+
+                policies.Add(new(int.Parse(min), int.Parse(max), letter, password.ToString()));
+            }
+
+
+
+            var resultCount = policies.Count(p => p.CheckPasswordAgainstCountPolicy());
+            Console.WriteLine(resultCount);
+
+
+            var resultPosition = policies.Count(p => p.CheckPasswordAgainstPositionPolicy());
+            Console.WriteLine(resultPosition);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day02/Policy.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+
+namespace Day02
+{
+    public class Policy
+    {
+        public int Min { get; }
+        public int Max { get; }
+
+        public char Letter { get; }
+
+        public string Password { get; }
+
+
+
+        public Policy(int min, int max, char letter, string password)
+        {
+            this.Min = min;
+            this.Max = max;
+            this.Letter = letter;
+            this.Password = password;
+        }
+
+
+
+        public bool CheckPasswordAgainstCountPolicy()
+        {
+            var count = this.Password.Count(c => c == this.Letter);
+
+            return count >= this.Min && count <= this.Max;
+        }
+
+        public bool CheckPasswordAgainstPositionPolicy()
+        {
+            return (this.Password[this.Min - 1] == this.Letter) ^ (this.Password[this.Max - 1] == this.Letter);
+        }
+    }
+}
