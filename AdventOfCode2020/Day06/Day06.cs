6eeb206b17ca1dc092d1891ea04a2ba1ae2cf0de
Tjaž Brelih <tjaz.brelih@gmail.com>
2020-12-06 09:09:10 +0100

    Day 6 solution.

---
+++ AdventOfCode2020/Day06/Day06.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+
+using var file = new StreamReader("input.txt");
+
+List<List<string>> groups = new();
+List<string> currentGroup = new();
+
+while (true)
+{
+    var line = file.ReadLine();
+
+    if (line == null || line.Length == 0)
+    {
+        groups.Add(currentGroup);
+
+        if (line == null)
+        {
+            break;
+        }
+
+        currentGroup = new();
+        continue;
+    }
+
+
+    currentGroup.Add(line);
+}
+
+
+var resultOne = groups.Sum(g => g.SelectMany(g => g.ToList()).Distinct().Count());
+Console.WriteLine(resultOne);
+
+
+var resultTwo = 0;
+foreach (var group in groups)
+{
+    var peopleCount = group.Count;
+
+    resultTwo += group.SelectMany(g => g.ToList()).GroupBy(c => c).Select(g => g.Count()).Where(c => c == peopleCount).Count();
+}
+Console.WriteLine(resultTwo);
