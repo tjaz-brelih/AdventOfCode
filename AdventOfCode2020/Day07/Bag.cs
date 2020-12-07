31af5bf7b2ccd31ea2376cfbb9745b35c5ae4b5f
Tjaž Brelih <tjaz.brelih@kolektor.com>
2020-12-07 11:49:22 +0100

    Day 7 part 1 solution.

---
+++ AdventOfCode2020/Day07/Bag.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Day07
+{
+    public class Bag
+    {
+        public string Name { get; }
+        public int Count { get; }
+
+        public List<Bag> ContainedBags { get; } = new();
+
+
+
+        public Bag(string name)
+        {
+            this.Name = name;
+            this.Count = 1;
+        }
+
+        public Bag(string name, int count)
+        {
+            this.Name = name;
+            this.Count = count;
+        }
+
+
+
+        public void Contains(string bagName, ICollection<Bag> bags)
+        {
+            if (this.Contains(bagName))
+            {
+                bags.Add(this);
+            }
+
+            foreach (var bag in this.ContainedBags)
+            {
+                bag.Contains(bagName, bags);
+            }
+        }
+
+
+        public bool Contains(string bagName) => this.ContainedBags.Any(b => b.Name == bagName || b.Contains(bagName));
+    }
+}
---
+++ AdventOfCode2020/Day07/Day07.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Day07;
+
+
+using var file = new StreamReader("input.txt");
+
+List<string> lines = new();
+HashSet<Bag> topLevelBags = new();
+
+while (file.ReadLine() is string line)
+{
+    lines.Add(line);
+}
+
+foreach (var line in lines)
+{
+    var bagTokens = line.Split(' ');
+
+    Bag currentBag = new(bagTokens[0] + " " + bagTokens[1]);
+    topLevelBags.Add(currentBag);
+}
+
+
+foreach (var line in lines)
+{
+    var bags = line.Split(',');
+    var bagTokens = bags[0].Split(' ');
+
+    if (bagTokens[4] == "no" && bagTokens[5] == "other")
+    {
+        continue;
+    }
+
+    var topLevelBag = topLevelBags.SingleOrDefault(b => b.Name == bagTokens[0] + " " + bagTokens[1]);
+    
+    var containedTopLevelBag = topLevelBags.Single(b => b.Name == bagTokens[5] + " " + bagTokens[6]);
+    topLevelBag.ContainedBags.Add(containedTopLevelBag);
+
+    foreach (var bag in bags.Skip(1))
+    {
+        bagTokens = bag.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        containedTopLevelBag = topLevelBags.Single(b => b.Name == bagTokens[1] + " " + bagTokens[2]);
+
+        topLevelBag.ContainedBags.Add(containedTopLevelBag);
+    }
+}
+
+
+List<Bag> goldBags = new();
+foreach (var bag in topLevelBags)
+{
+    bag.Contains("shiny gold", goldBags);
+}
+
+Console.WriteLine(goldBags.Where(b => b.Name != "shiny gold").Select(b => b.Name).Distinct().Count());
