46ef11ec7c92a2cd03a72bc18be15cbe2a65bb21
Tjaž Brelih <tjaz.brelih@gmail.com>
2020-12-13 10:42:44 +0100

    Day 13 part 1 solution.

---
+++ AdventOfCode2020/Day13/Day13.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+
+using var file = new StreamReader("input2.txt");
+
+var earliest = int.Parse(file.ReadLine());
+var busIds = file.ReadLine().Split(',').AsEnumerable();
+
+
+
+var resultOne = busIds
+    .Where(b => b != "x")
+    .Select(b => new
+    {
+        Id = int.Parse(b),
+        Arrival = ((decimal) earliest / int.Parse(b))
+    })
+    .OrderByDescending(b => b.Arrival % 1)
+    .First();
+
+Console.WriteLine(resultOne.Id * ((resultOne.Id * Math.Ceiling(resultOne.Arrival)) - earliest));
+
+
+
