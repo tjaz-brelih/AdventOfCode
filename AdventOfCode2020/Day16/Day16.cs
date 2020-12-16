2d41ff667b92d415b1709a659229feba344b81ac
Tjaž Brelih <tjaz.brelih@kolektor.com>
2020-12-16 08:32:00 +0100

    Day 16 part 1 solution, part 2 WIP.

---
+++ AdventOfCode2020/Day16/Day16.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Day16;
+
+
+
+using var file = new StreamReader("input.txt");
+
+List<Rule> rules = new();
+
+while (file.ReadLine() is string line)
+{
+    if (line.Trim().Length == 0)
+    {
+        break;
+    }
+
+    rules.Add(Rule.FromString(line));
+}
+
+
+file.ReadLine();
+var myTicket = file.ReadLine().Split(',').Select(s => int.Parse(s)).ToList();
+
+file.ReadLine();
+file.ReadLine();
+
+
+List<List<int>> nearbyTickets = new();
+
+while (file.ReadLine() is string line)
+{
+    nearbyTickets.Add(line.Split(',').Select(s => int.Parse(s)).ToList());
+}
+
+
+
+var resultOne = nearbyTickets
+    .SelectMany(t => t.Select(f => f))
+    .Where(f => !rules.Any(r => r.InAnyRange(f)))
+    .Sum();
+
+Console.WriteLine(resultOne);
+
+
+
+var validTickets = nearbyTickets
+    .Where(t => t.All(f => rules.Any(r => r.InAnyRange(f))));
+
+Dictionary<Rule, int> ruleColumn = new();
+
+while (true)
+{
+    var unknownRules = rules.Where(r => !ruleColumn.ContainsKey(r));
+
+    if (!unknownRules.Any())
+    {
+        break;
+    }
+
+    foreach (var rule in unknownRules)
+    {
+        var ruleConforms = false;
+
+        for (int i = 0; i < validTickets.First().Count; i++)
+        {
+            ruleConforms = validTickets.Select(t => t[i]).All(f => rule.InAnyRange(f));
+
+            if (ruleConforms)
+            {
+                ruleColumn[rule] = i;
+                break;
+            }
+        }
+
+        if (ruleConforms)
+        {
+            break;
+        }
+    }
+}
+
+var resultTwo = ruleColumn
+    .Where(kv => kv.Key.Name.StartsWith("direction"))
+    .Aggregate(1UL, (acc, kv) => acc * (ulong) myTicket[kv.Value]);
+
+Console.WriteLine(resultTwo);
---
+++ AdventOfCode2020/Day16/Rule.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Day16
+{
+    public class Rule
+    {
+        public string Name { get; }
+
+        public List<Range> Ranges { get; } = new();
+
+
+
+        public Rule(string name)
+        {
+            this.Name = name;
+        }
+
+
+        public static Rule FromString(string input)
+        {
+            var colonIndex = input.IndexOf(':');
+
+            var name = input[0..colonIndex];
+            var rule = new Rule(name);
+
+            var tokens = input[(colonIndex + 2)..].Split(' ');
+
+            rule.Ranges.Add(Range.FromString(tokens[0]));
+            rule.Ranges.Add(Range.FromString(tokens[2]));
+
+            return rule;
+        }
+
+
+
+        public bool InAnyRange(int value) => this.Ranges.Any(r => r.IsInRange(value));
+    }
+
+
+    public class Range
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+
+
+        public Range(int start, int end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+
+        public static Range FromString(string input)
+        {
+            var hyphenIndex = input.IndexOf('-');
+
+            return new Range(int.Parse(input[0..hyphenIndex]), int.Parse(input[(hyphenIndex + 1)..]));
+        }
+
+
+
+        public bool IsInRange(int value) => value >= this.Start && value <= this.End;
+    }
+}
