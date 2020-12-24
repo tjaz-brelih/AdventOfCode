21f3769a791f4f4dbc2fe3129abab7b1b2fac463
Tjaž Brelih <tjaz.brelih@gmail.com>
2020-12-24 15:11:03 +0100

    Solved day 24.

--- AdventOfCode2020/Day24/Day24.cs
+++ AdventOfCode2020/Day24/Day24.cs
@@ -16,7 +16,7 @@ while (file.ReadLine() is string line)
 
 
 
-HashSet<(int, int)> floor = new();
+HashSet<(int, int)> blackTiles = new();
 
 foreach (var line in lines)
 {
@@ -60,10 +60,80 @@ foreach (var line in lines)
         coordinates.Y += dY;
     }
 
-    _ = floor.Contains(coordinates) ? floor.Remove(coordinates) : floor.Add(coordinates);
+    _ = blackTiles.Contains(coordinates) ? blackTiles.Remove(coordinates) : blackTiles.Add(coordinates);
 }
 
-var resultOne = floor.Count;
+var resultOne = blackTiles.Count;
 Console.WriteLine(resultOne);
 
 
+
+Dictionary<(int X, int Y), bool> floor = new( blackTiles.Select(t => new KeyValuePair<(int, int), bool>(t, true) ));
+Dictionary<(int X, int Y), bool> floorPrev = new();
+
+for (int eh = 0; eh < 100; eh++)
+{
+    CloneDictionary(floor, floorPrev);
+
+    (int minX, int minY) = (int.MaxValue, int.MaxValue);
+    (int maxX, int maxY) = (int.MinValue, int.MinValue);
+
+    foreach (var state in floor)
+    {
+        (int X, int Y) = state.Key;
+
+        if (X < minX)
+            minX = X;
+        if (X > maxX)
+            maxX = X;
+
+        if (Y < minY)
+            minY = Y;
+        if (Y > maxY)
+            maxY = Y;
+    }
+
+    for (int i = minX - 1; i <= maxX + 1; i++)
+    {
+        for (int j = minY - 1; j <= maxY + 1; j++)
+        {
+            var currentState = floorPrev.TryGetValue((i, j), out var state) && state;
+            var activeStates = GenerateNeighbours(i, j).Count(n => floorPrev.GetValueOrDefault((n.Item1, n.Item2), false));
+
+            floor[(i, j)] = currentState switch
+            {
+                true => activeStates == 1 || activeStates == 2,
+                false => activeStates == 2
+            };
+        }
+    }
+}
+
+var resultTwo = floor.Count(kv => kv.Value);
+Console.WriteLine(resultTwo);
+
+
+
+
+static IEnumerable<(int, int)> GenerateNeighbours(int X, int Y)
+{
+    yield return (X + 1, Y);
+    yield return (X - 1, Y);
+
+    yield return (X + 1, Y - 1);
+    yield return (X, Y - 1);
+
+    yield return (X, Y + 1);
+    yield return (X - 1, Y + 1);
+}
+
+
+static void CloneDictionary<Tk, Tv>(Dictionary<Tk, Tv> input, Dictionary<Tk, Tv> output)
+{
+    output.Clear();
+
+    foreach (var kv in input)
+    {
+        output.Add(kv.Key, kv.Value);
+    }
+}
