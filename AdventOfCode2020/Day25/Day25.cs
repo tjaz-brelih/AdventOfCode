afe5a55b2f999c5c4c736d7c7f06b777c7282e9b
Tjaž Brelih <tjaz.brelih@gmail.com>
2020-12-25 10:49:46 +0100

    Day 25 solution.

---
+++ AdventOfCode2020/Day25/Day25.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+
+
+using var file = new StreamReader("input.txt");
+
+var pkCard = ulong.Parse(file.ReadLine());
+var pkDoor = ulong.Parse(file.ReadLine());
+
+
+
+var divider = 20201227U;
+var subjectNumber = 7U;
+
+var loopSizeCard = FindLoopSize(pkCard, divider, subjectNumber);
+var encryptionKeyCard = GenerateEncryptionKey(pkDoor, loopSizeCard, divider);
+Console.WriteLine(encryptionKeyCard);
+
+
+
+
+static ulong FindLoopSize(ulong pk, ulong divider, ulong subjectNumber, ulong value = 1U)
+{
+    var loopSize = 0U;
+
+    while (value != pk)
+    {
+        value *= subjectNumber;
+        value %= divider;
+
+        loopSize++;
+    }
+
+    return loopSize;
+}
+
+
+static ulong GenerateEncryptionKey(ulong pk, ulong loopSize, ulong divider, ulong value = 1U)
+{
+    for (ulong i = 0; i < loopSize; i++)
+    {
+        value *= pk;
+        value %= divider;
+    }
+
+    return value;
+}
