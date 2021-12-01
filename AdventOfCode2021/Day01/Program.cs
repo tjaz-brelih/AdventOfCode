ed183784fba4c8f63151d4f854467d1eab5d908d
Tjaž Brelih <tjaz.brelih@gmail.com>
2021-12-01 14:08:46 +0100

    2021: Day 01.

---
+++ AdventOfCode2021/Day01/Program.cs
@@ -0,0 +1,33 @@
+﻿using var file = new StreamReader("input.txt");
+
+List<uint> lines = new();
+
+while (file.ReadLine() is string line)
+{
+    lines.Add(uint.Parse(line));
+}
+
+
+
+var zip = lines.Append(uint.MaxValue).Zip(lines.Skip(1));
+
+Console.WriteLine(zip.Count(t => t.Second > t.First));
+
+
+
+uint prevSum = 0;
+int largerCount = -1;
+
+for (int i = 0; i < lines.Count - 2; i++)
+{
+    var curSum = lines[i] + lines[i + 1] + lines[i + 2];
+
+    if (curSum > prevSum)
+    {
+        largerCount++;
+    }
+    
+    prevSum = curSum;
+}
+
+Console.WriteLine(largerCount);
