f7a1d32084306f3ad496c8b572e68a6252a5dbe1
Tjaž Brelih <tjaz.brelih@gmail.com>
2021-12-01 13:47:27 +0100

    Day 20 WIP.

---
+++ AdventOfCode2020/Day20/Day20.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using Day20;
+
+
+
+using var file = new StreamReader("input2.txt");
+
+List<Tile> tiles = new();
+
+while (file.ReadLine() is string line)
+{
+    if (line == string.Empty)
+    {
+        continue;
+    }
+
+    var colonIndex = line.IndexOf(':');
+
+    var tileNumber = int.Parse(line.AsSpan()[5..colonIndex]);
+
+    char[,] tileData = new char[10,10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        var data = file.ReadLine();
+        for (int j = 0; j < 10; j++)
+        {
+            tileData[i, j] = data[j];
+        }
+    }
+
+    tiles.Add(new Tile(tileNumber, tileData));
+}
+
+
+
+//(int X, int Y) coordinates = (0, 0);
+//Dictionary<(int, int), Tile> image = new()
+//{
+//    { coordinates, tiles.First() }
+//};
+
+//var tilesLeft = tiles.ToHashSet();
+//tilesLeft.Remove(tiles.First());
+
+//foreach (var tile in tilesLeft)
+//{
+
+//}
+
+var resultOne = tiles
+    .Where(t => t.NeighboursCount == 2)
+    .Aggregate(1UL, (acc, t) => acc * (ulong)t.Name);
+Console.WriteLine(resultOne);
+
+
+
+static bool EqualsAny(int value, (int A, int B, int C, int D) tuple)
+    => value == tuple.A || value == tuple.B || value == tuple.C || value == tuple.D;
---
+++ AdventOfCode2020/Day20/Tile.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+
+namespace Day20
+{
+    public class Tile
+    {
+        private (string Top, string Bottom, string Left, string Right) _corners;
+
+
+        public int Name { get; }
+        public char[,] Data { get; }
+
+        public int NeighboursCount { get; set; }
+
+        public (string Top, string Bottom, string Left, string Right) Corners => this._corners;
+
+
+
+        public Tile(int name, char[,] data)
+        {
+            this.Name = name;
+            this.Data = data;
+
+            this.PopulateCorners();
+        }
+
+
+
+        private void PopulateCorners()
+        {
+            StringBuilder builder = new();
+
+            for (int i = 0; i < this.Data.GetLength(1); i++)
+            {
+                builder.Append(this.Data[0, i]);
+            }
+            
+            this._corners.Top = builder.ToString();
+            builder.Clear();
+
+
+            for (int i = 0; i < this.Data.GetLength(1); i++)
+            {
+                builder.Append(this.Data[this.Data.GetLength(1) - 1, i]);
+            }
+            
+            this._corners.Bottom = builder.ToString();
+            builder.Clear();
+
+
+            for (int i = 0; i < this.Data.GetLength(0); i++)
+            {
+                builder.Append(this.Data[i, 0]);
+            }
+
+            this._corners.Left = builder.ToString();
+            builder.Clear();
+
+            
+            for (int i = 0; i < this.Data.GetLength(0); i++)
+            {
+                builder.Append(this.Data[i, this.Data.GetLength(0) - 1]);
+            }
+
+            this._corners.Right = builder.ToString();
+        }
+    }
+}
