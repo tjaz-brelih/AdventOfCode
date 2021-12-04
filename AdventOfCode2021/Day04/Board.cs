dee0860a5bb868f7c542e699e6103eefabe016d7
Tjaž Brelih <tjaz.brelih@gmail.com>
2021-12-04 11:59:54 +0100

    Day 4 solution.

---
+++ AdventOfCode2021/Day04/Board.cs
@@ -0,0 +1,60 @@
+﻿public class Board
+{
+    public int[,] Numbers { get; }
+
+    public int[] Rows { get; }
+    public int[] Columns { get; }
+
+    public bool IsComplete => this.Rows.Any(r => r == this.Numbers.GetLength(0)) || this.Columns.Any(c => c == this.Numbers.GetLength(0));
+
+
+
+    public Board(int[,] numbers)
+    {
+        this.Numbers = numbers;
+        this.Rows = new int[numbers.GetLength(0)];
+        this.Columns = new int[numbers.GetLength(0)];
+    }
+
+
+
+    public void Reset()
+    {
+        for (int i = 0; i < this.Numbers.GetLength(0); i++)
+        {
+            this.Rows[i] = 0;
+            this.Columns[i] = 0;
+        }
+    }
+
+
+    public bool MarkNumber(int number)
+    {
+        for (int i = 0; i < this.Numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < this.Numbers.GetLength(1); j++)
+            {
+                if (this.Numbers[i, j] == number)
+                {
+                    this.Rows[i]++;
+                    this.Columns[j]++;
+                }
+            }
+        }
+
+        return this.IsComplete;
+    }
+
+    public int Score(IEnumerable<int> drawnNumbers)
+    {
+        var sum = 0;
+        foreach (var number in this.Numbers)
+        {
+            if (!drawnNumbers.Contains(number))
+            {
+                sum += number;
+            }
+        }
+        return sum;
+    }
+}
---
+++ AdventOfCode2021/Day04/Program.cs
@@ -0,0 +1,87 @@
+﻿using var file = new StreamReader("input.txt");
+
+var drawingNumbers = file.ReadLine()!.Split(',').Select(n => int.Parse(n)).ToList();
+
+List<Board> boards = new();
+Board? currentBoard = null;
+int currentPosition = 0;
+
+while (file.ReadLine() is string line)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        currentPosition = 0;
+        currentBoard = new(new int[5, 5]);
+        boards.Add(currentBoard);
+    }
+    else
+    {
+        var array = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < array.Length; i++)
+        {
+            currentBoard!.Numbers[currentPosition, i] = int.Parse(array[i]);
+        }
+
+        currentPosition++;
+    }
+}
+
+
+
+int finalNumber = 0;
+Board? winningBoard = null;
+HashSet<int> drawnNumbers = new();
+
+foreach (var number in drawingNumbers)
+{
+    drawnNumbers.Add(number);
+
+    foreach (var board in boards)
+    {
+        if (board.MarkNumber(number))
+        {
+            winningBoard = board;
+            break;
+        }
+    }
+
+    if (winningBoard is not null)
+    {
+        finalNumber = number;
+        break;
+    }
+}
+
+var resultOne = winningBoard!.Score(drawnNumbers) * finalNumber;
+Console.WriteLine(resultOne);
+
+
+
+foreach (var board in boards)
+{
+    board.Reset();
+}
+
+drawnNumbers = new();
+
+foreach (var number in drawingNumbers)
+{
+    drawnNumbers.Add(number);
+
+    foreach (var board in boards.Where(b => !b.IsComplete))
+    {
+        if (board.MarkNumber(number))
+        {
+            winningBoard = board;
+        }
+    }
+
+    if (boards.All(b => b.IsComplete))
+    {
+        finalNumber = number;
+        break;
+    }
+}
+
+var resultTwo = winningBoard!.Score(drawnNumbers) * finalNumber;
+Console.WriteLine(resultTwo);
