e6b233e59c9ca12481c56cc8ec66e85adc3831a5
Tjaž Brelih <tjaz.brelih@gmail.com>
2021-12-13 13:50:53 +0100

    Day 12 solution.

---
+++ AdventOfCode2021/Day12/Cave.cs
@@ -0,0 +1,18 @@
+﻿namespace Day12
+{
+    internal class Cave
+    {
+        public string Name { get; }
+        public bool IsSmall { get; }
+
+        public List<Cave> ConnectedCaves { get; } = new();
+
+
+
+        public Cave(string name)
+        {
+            this.Name = name;
+            this.IsSmall = char.IsLower(this.Name[0]);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day12/Program.cs
@@ -0,0 +1,75 @@
+﻿using Day12;
+
+
+using var file = new StreamReader("input.txt");
+
+List<Cave> caves = new();
+
+while (file.ReadLine() is string line)
+{
+    var tokens = line.Split('-');
+
+    var cave1 = caves.SingleOrDefault(c => c.Name == tokens[0]);
+    if (cave1 is null)
+    {
+        cave1 = new Cave(tokens[0]);
+        caves.Add(cave1);
+    }
+
+    var cave2 = caves.SingleOrDefault(c => c.Name == tokens[1]);
+    if (cave2 is null)
+    {
+        cave2 = new Cave(tokens[1]);
+        caves.Add(cave2);
+    }
+
+    cave1.ConnectedCaves.Add(cave2);
+    cave2.ConnectedCaves.Add(cave1);
+}
+
+
+
+var resultOne = TraverseCaves(caves.Single(c => c.Name == "start"), new());
+Console.WriteLine(resultOne);
+
+
+
+var resultTwo = TraverseCaves2(caves.Single(c => c.Name == "start"), new());
+Console.WriteLine(resultTwo);
+
+
+
+static int TraverseCaves(Cave cave, List<Cave> path)
+{
+    if (path.Contains(cave) && cave.IsSmall)
+    {
+        return 0;
+    }
+
+    if (cave.Name == "end")
+    {
+        return 1;
+    }
+
+    path.Add(cave);
+    return cave.ConnectedCaves.Where(c => c.Name != "start").Sum(c => TraverseCaves(c, path!.ToList()));
+}
+
+
+static int TraverseCaves2(Cave cave, List<Cave> path, bool hasVisitedSmallCave = false)
+{
+    if (path.Contains(cave) && cave.IsSmall && hasVisitedSmallCave)
+    {
+        return 0;
+    }
+
+    if (cave.Name == "end")
+    {
+        return 1;
+    }
+
+    hasVisitedSmallCave = hasVisitedSmallCave || path.Contains(cave) && cave.IsSmall;
+    path.Add(cave);
+
+    return cave.ConnectedCaves.Where(c => c.Name != "start").Sum(c => TraverseCaves2(c, path!.ToList(), hasVisitedSmallCave));
+}
