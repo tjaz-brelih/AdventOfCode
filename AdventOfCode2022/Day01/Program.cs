0b3fd7bc47b7643e43f739d05b8e8f1c93678477
Tjaž Brelih <tjaz.brelih@kolektor.com>
2022-12-01 07:38:31 +0100

    Day 1 solution.

--- AdventOfCode2022/Day01/Program.cs
+++ AdventOfCode2022/Day01/Program.cs
@@ -1,8 +1,29 @@
 ﻿using var file = new StreamReader("input.txt");
 
-List<int> lines = new();
+List<List<int>> elves = new();
+
+List<int> currentList = new();
+
+elves.Add(currentList);
 
 while (file.ReadLine() is string line)
 {
-    lines.Add(int.Parse(line));
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        currentList = new();
+        elves.Add(currentList);
+
+        continue;
+    }
+
+    currentList.Add(int.Parse(line));
 }
+
+
+// Part 1
+var resultOne = elves.Max(x => x.Sum());
+Console.WriteLine(resultOne);
+
+// Part 2
+var resultTwo = elves.Select(x => x.Sum()).OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine(resultTwo);
