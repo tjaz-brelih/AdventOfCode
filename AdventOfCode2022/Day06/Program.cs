8ddb868b767716fed256d1828089b8428805bcae
Tjaž Brelih <tjaz.brelih@gmail.com>
2022-12-06 07:58:58 +0100

    Day 6 solution.

--- AdventOfCode2022/Day06/Program.cs
+++ AdventOfCode2022/Day06/Program.cs
@@ -6,3 +6,65 @@ while (file.ReadLine() is string line)
 {
     input.Add(line);
 }
+
+
+// Part 1
+var resultOne = 0;
+var length = 4;
+var set = new HashSet<char>();
+
+foreach (var line in input)
+{
+    var span = line.AsSpan();
+
+    for (int i = 0; i < span.Length - length; i++)
+    {
+        set.Clear();
+        var subSpan = span[i..(i + length)];
+        FillSet(set, subSpan);
+
+        if (set.Count == subSpan.Length)
+        {
+            resultOne = i + length;
+            break;
+        }
+    }
+
+    Console.WriteLine(resultOne);
+}
+
+
+// Part 2
+var resultTwo = 0;
+length = 14;
+
+foreach (var line in input)
+{
+    var span = line.AsSpan();
+
+    for (int i = 0; i < span.Length - length; i++)
+    {
+        set.Clear();
+        var subSpan = span[i..(i + length)];
+        FillSet(set, subSpan);
+
+        if (set.Count == subSpan.Length)
+        {
+            resultTwo = i + length;
+            break;
+        }
+    }
+
+    Console.WriteLine(resultTwo);
+}
+
+
+
+
+static void FillSet<T>(HashSet<T> set, ReadOnlySpan<T> input)
+{
+    foreach (var c in input)
+    {
+        set.Add(c);
+    }
+}
