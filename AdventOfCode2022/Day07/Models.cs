e6effc85dfd4193db923419db49e5df518b283d5
Tjaž Brelih <tjaz.brelih@gmail.com>
2022-12-07 12:07:23 +0100

    Day 7 solution.

---
+++ AdventOfCode2022/Day07/Models.cs
@@ -0,0 +1,101 @@
+﻿namespace Day07;
+
+
+public class DirectoryAoc
+{
+    private ulong? _size;
+
+    public string Name { get; }
+
+    public DirectoryAoc? Parent { get; set; }
+
+    public List<DirectoryAoc> Directories { get; } = new();
+    public List<FileAoc> Files { get; } = new();
+
+    public ulong Size => this.GetSize();
+
+
+
+    public DirectoryAoc(string name)
+    {
+        this.Name = name;
+    }
+
+
+
+    private ulong GetSize()
+    {
+        this._size ??= this.CalculateSize();
+        return this._size.Value;
+    }
+
+    private ulong CalculateSize()
+        => this.Directories.Aggregate(0ul, (acc, x) => acc + x.Size) + this.Files.Aggregate(0ul, (acc, x) => acc + x.Size);
+
+    public DirectoryAoc? GetSubdirectory(ReadOnlySpan<char> name)
+    {
+        if (this.Name == name)
+        {
+            return this;
+        }
+
+        foreach (var directory in this.Directories)
+        {
+            return directory.GetSubdirectory(name);
+        }
+
+        return null;
+    }
+
+    public DirectoryAoc AddSubdirectory(ReadOnlySpan<char> name)
+    {
+        // We can't use ReadOnlySpan in lambda function, therefore we cannot use LINQ.
+        foreach (var directory in this.Directories)
+        {
+            if (directory.Name == name)
+            {
+                return directory;
+            }
+        }
+
+        var newDirectory = new DirectoryAoc(name.ToString())
+        {
+            Parent = this
+        };
+
+        this.Directories.Add(newDirectory);
+
+        return newDirectory;
+    }
+
+    public FileAoc AddFile(ReadOnlySpan<char> name, ulong size)
+    {
+        // We can't use ReadOnlySpan in lambda function, therefore we cannot use LINQ.
+        foreach (var file in this.Files)
+        {
+            if (file.Name == name)
+            {
+                return file;
+            }
+        }
+
+        var newFile = new FileAoc(name.ToString(), size);
+        this.Files.Add(newFile);
+        return newFile;
+    }
+}
+
+
+public class FileAoc
+{
+    public string Name { get; }
+    public ulong Size { get; }
+
+
+
+    public FileAoc(string name, ulong size)
+    {
+        this.Name = name;
+        this.Size = size;
+    }
+}
--- AdventOfCode2022/Day07/Program.cs
+++ AdventOfCode2022/Day07/Program.cs
@@ -1,4 +1,6 @@
-﻿using var file = new StreamReader("input.txt");
+﻿using Day07;
+
+using var file = new StreamReader("input.txt");
 
 List<string> input = new();
 
@@ -9,6 +11,65 @@ while (file.ReadLine() is string line)
 
 
 // Part 1
+DirectoryAoc topDirectory = new("/");
+DirectoryAoc currentDirectory = topDirectory;
+
+foreach (var line in input)
+{
+    var span = line.AsSpan();
+
+    if (span.StartsWith("$ cd"))
+    {
+        currentDirectory = span[5] switch
+        {
+            '/' => topDirectory,
+            '.' => currentDirectory.Parent ?? currentDirectory,
+            _ => currentDirectory.AddSubdirectory(span[5..])
+        };
+    }
+    // File output
+    else if (char.IsAsciiDigit(span[0]))
+    {
+        var spaceIndex = span.IndexOf(' ');
+
+        var size = span[..spaceIndex];
+        var name = span[(spaceIndex + 1)..];
+
+        currentDirectory.AddFile(name, ulong.Parse(size));
+    }
+}
+
+var directoryList = new List<DirectoryAoc>();
+FilterDirectories(directoryList, topDirectory, 100000);
+
+var resultOne = directoryList.Aggregate(0ul, (acc, x) => acc + x.Size);
+
+Console.WriteLine(resultOne);
 
 
 // Part 2
+var deleteSize = 30000000ul - (70000000ul - topDirectory.Size);
+directoryList = new();
+FilterDirectories(directoryList, topDirectory, ulong.MaxValue);
+
+var resultTwo = directoryList
+    .Where(x => x.Size >= deleteSize)
+    .Min(x => x.Size);
+
+Console.WriteLine(resultTwo);
+
+
+
+
+static void FilterDirectories(List<DirectoryAoc> directories, DirectoryAoc current, ulong maxSize)
+{
+    if (current.Size <= maxSize)
+    {
+        directories.Add(current);
+    }
+
+    foreach (var directory in current.Directories)
+    {
+        FilterDirectories(directories, directory, maxSize);
+    }
+}
