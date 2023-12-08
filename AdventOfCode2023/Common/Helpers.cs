d47ed62308bb9be55998b84da083285b163472e8
Tjaž Brelih <tjaz.brelih@gmail.com>
2023-12-08 11:10:52 +0100

    Added helper method for LCM and GCD.

--- AdventOfCode2023/Common/Helpers.cs
+++ AdventOfCode2023/Common/Helpers.cs
@@ -1,4 +1,6 @@
-﻿namespace Common;
+﻿using System.Numerics;
+
+namespace Common;
 
 public static class Helpers
 {
@@ -14,5 +16,41 @@ public static class Helpers
         }
 
         return lines;
+    }
+
+
+    public static T LeastCommonMultiple<T>(IEnumerable<T> a) where T : INumber<T>
+        => LcmGcdInternal(a, (a, b) => LeastCommonMultiple(a, b));
+
+    public static T GreatestCommonDivisor<T>(IEnumerable<T> a) where T : INumber<T>
+        => LcmGcdInternal(a, (a, b) => GreatestCommonDivisor(a, b));
+
+
+    private static T LcmGcdInternal<T>(IEnumerable<T> a, Func<T, T, T> f) where T : INumber<T>
+    {
+        var stack = new Stack<T>(a);
+
+        if (stack.Count == 0) throw new ArgumentException("Empty collection", nameof(a));
+        while (stack.Count > 1) stack.Push(f(stack.Pop(), stack.Pop()));
+
+        return stack.Pop();
+    }
+
+
+    public static T LeastCommonMultiple<T>(T a, T b) where T : INumber<T>
+    {
+        return a * b / GreatestCommonDivisor(a, b);
+    }
+
+    public static T GreatestCommonDivisor<T>(T a, T b) where T : INumber<T>
+    {
+        while (b != T.Zero)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
     }
 }
