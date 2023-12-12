3c81a173692230d529511e0b18919781cc215c6c
Tjaž Brelih <tjaz.brelih@gmail.com>
2023-12-12 10:24:13 +0100

    Day 12 wip.

---
+++ AdventOfCode2023/Day12/Program.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+var lines = Helpers.ReadInputFile();
+
+
+foreach (var line in lines)
+{
+    var index = line.IndexOf(' ');
+
+    var springs = line.AsSpan()[..index].ToArray();
+    var groups = line[(index + 1)..].Split(',').Select(int.Parse).ToArray();
+
+
+}
