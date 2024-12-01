602c4f36713e8a05887c932526cd658bb29a29eb
Tjaž Brelih <tjaz.brelih@gmail.com>
2024-12-01 09:49:50 +0100

    AoC 24 and day 1 solution.

---
+++ AdventOfCode2024/Common/Helpers.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace Common;
+
+public static class Helpers
+{
+    public static List<string> ReadInputFile(string filePath = "input.txt")
+    {
+        using var file = new StreamReader(filePath);
+
+        List<string> lines = [];
+
+        while (file.ReadLine() is string line)
+        {
+            lines.Add(line);
+        }
+
+        return lines;
+    }
+
+
+    public static T LeastCommonMultiple<T>(IEnumerable<T> a) where T : INumber<T>
+        => LcmGcdInternal(a, (a, b) => LeastCommonMultiple(a, b));
+
+    public static T GreatestCommonDivisor<T>(IEnumerable<T> a) where T : INumber<T>
+        => LcmGcdInternal(a, (a, b) => GreatestCommonDivisor(a, b));
+
+
+    private static T LcmGcdInternal<T>(IEnumerable<T> a, Func<T, T, T> f) where T : INumber<T>
+    {
+        var stack = new Stack<T>(a);
+
+        if (stack.Count == 0) throw new ArgumentException("Empty collection", nameof(a));
+        while (stack.Count > 1) stack.Push(f(stack.Pop(), stack.Pop()));
+
+        return stack.Pop();
+    }
+
+
+    public static T LeastCommonMultiple<T>(T a, T b) where T : INumber<T>
+    {
+        return a * b / GreatestCommonDivisor(a, b);
+    }
+
+    public static T GreatestCommonDivisor<T>(T a, T b) where T : INumber<T>
+    {
+        while (b != T.Zero)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
---
+++ AdventOfCode2024/Day01/Program.cs
@@ -0,0 +1,39 @@
+﻿using var file = new StreamReader("input.txt");
+
+
+List<int> list1 = [];
+List<int> list2 = [];
+
+while (file.ReadLine() is string line)
+{
+    var span = line.AsSpan();
+
+    var start = span.IndexOf(' ');
+    var end = span.LastIndexOf(' ');
+
+    list1.Add(int.Parse(span[..start]));
+    list2.Add(int.Parse(span[(end + 1)..]));
+}
+
+
+list1.Sort();
+list2.Sort();
+
+var resultOne = list1.Zip(list2).Sum(x => Math.Abs(x.First - x.Second));
+
+Console.WriteLine(resultOne);
+
+
+
+var counter = new Dictionary<int, int>();
+
+foreach (var item in list1)
+{
+    if (counter.ContainsKey(item)) { continue; }
+
+    counter[item] = list2.Count(x => x == item);
+}
+
+var resultTwo = list1.Sum(x => x * counter[x]);
+
+Console.WriteLine(resultTwo);
