06f2d04866467e7f7e045ee55f5c7047fe6b0dfb
Tjaž Brelih <tjaz.brelih@kolektor.com>
2024-12-19 12:44:18 +0100

    Day 19 solution.

---
+++ AdventOfCode2024/Day19/Program.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+
+var lines = Helpers.ReadInputFile();
+
+var towels = lines[0].Split(", ");
+var designs = lines[2..];
+
+
+var resultOne = designs.Count(x => FindMatches(x.AsMemory(), towels, []) > 0);
+Console.WriteLine(resultOne);
+
+var resultTwo = designs.Sum(x => FindMatches(x.AsMemory(), towels, []));
+Console.WriteLine(resultTwo);
+
+
+
+static long FindMatches(ReadOnlyMemory<char> design, IList<string> towels, Dictionary<ReadOnlyMemory<char>, long> cache)
+{
+    if (design.Length == 0) { return 1; }
+    if (cache.TryGetValue(design, out var matches)) { return matches; }
+
+    var count = towels.Sum(x => design.Span.StartsWith(x) ? FindMatches(design[x.Length..], towels, cache) : 0);
+    cache[design] = count;
+
+    return count;
+}
