cc0eb130bb8af4bcb418e472ff93fe7e5fa14d31
Tjaž Brelih <6746788+tjaz-brelih@users.noreply.github.com>
2025-12-12 10:34:58 +0100

    Day 7 part 1 solution.

---
+++ AdventOfCode2025/Day07/Day07.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+
+var lines = Helpers.ReadInputFile();
+
+
+var resultOne = 0;
+HashSet<int> beams = [lines[0].IndexOf('S')];
+
+foreach (var line in lines)
+{
+    foreach (var beam in beams.Where(x => line[x] == '^').ToList())
+    {
+        beams.Add(beam - 1);
+        beams.Add(beam + 1);
+        beams.Remove(beam);
+
+        resultOne += 1;
+    }
+}
+
+Console.WriteLine(resultOne);
