1b21fa975d15bb56cf803193bd3ba67bc788dcaa
Tjaž Brelih <6746788+tjaz-brelih@users.noreply.github.com>
2025-12-15 14:23:01 +0100

    Day 9 part 1 solution.

---
+++ AdventOfCode2025/Day09/Day09.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using Coord = (long X, long Y);
+
+var lines = Helpers.ReadInputFile();
+
+var tiles = lines
+    .Select(x => x.Split(','))
+    .Select(x => (Coord) (long.Parse(x[0]), long.Parse(x[1])))
+    .ToList();
+
+
+var resultOne = 0L;
+
+for (var i = 0; i < tiles.Count - 1; i++)
+{
+    for (var j = i + 1; j < tiles.Count; j++)
+    {
+        var area = (Math.Abs(tiles[i].X - tiles[j].X) + 1) * (Math.Abs(tiles[i].Y - tiles[j].Y) + 1);
+        if (area > resultOne) { resultOne = area; }
+    }
+}
+
+Console.WriteLine(resultOne);
